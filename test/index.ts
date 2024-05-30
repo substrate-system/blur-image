60fa5e32b1ec2034f313be9dac78a79008dc76d4
Nick Thomas <nichoth@nichoth.com>
2024-05-30 01:00:22 -0700

    better test

--- test/index.ts
+++ test/index.ts
@@ -8,4 +8,9 @@ test('example', async t => {
     const srcset = el?.getAttribute('srcset')
     t.ok(srcset?.includes('100.jpg'), 'should render with a srcset attribute')
     t.ok(srcset?.includes('800w'), 'should render with a srcset attribute')
+    const img = el?.querySelector('img')
+    t.equal(img?.getAttribute('loading'), 'lazy',
+        'should have loading=lazy attribute')
+    t.equal(img?.getAttribute('decoding'), 'async',
+        'should have async decoding attribute')
 })
